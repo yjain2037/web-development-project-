26016951672f371d67704d020ff3c3890d5cde84
Yash Jain <yjain2037@gmail.com>
2025-04-02 17:39:50 +0530

    Delete musicLibrary.py

--- musicLibrary.py
+++
@@ -1,5 +0,0 @@
-music = {
-   "kali gaadi":" https://www.youtube.com/watch?v=M5mjNDwnBwo&ab_channel=MasoomSharma",
-   "om":" https://www.youtube.com/watch?v=M5mjNDwnBwo&ab_channel=MasoomSharma",
-   "jhol":"https://www.youtube.com/watch?v=BWczaSneA0Q&ab_channel=Maanu"
-}
